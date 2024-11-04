54c1671b0a402d18364aa99a967cc9bb3a51e930
plj saveli <saveli.pljasunov@gmail.com>
2024-11-04 11:07:54 +0200

    korjasin delete

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
- const date = new Date();
+app.use(express.static('dist'));
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
 
- app.use(express.static('dist'))
- app.use(cors());
- app.use(express.json());
- app.use(morgan('tiny'));
-
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -36,51 +34,48 @@ const persons = [
     }
 ];
 
+// Generate unique ID
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0;
+    return maxId + 1;
+};
+
+// Get all persons
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
-              <p>${date}</p>`);
-});
-
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id;
-    const person = persons.find(person => person.id === id);
-    if (!person) {
-      res.status(404).send(`Person ${id} not found`);
-    }
-    res.json(person);
-});
-
+// Add a new person
 app.post('/api/persons', (req, res) => {
     const body = req.body;
+
     if (!body.name || !body.number) {
-      return res.status(400).send('Name or number missing');
+        return res.status(400).json({ 
+            error: 'name or number missing' 
+        });
     }
-    if (persons.find(person => person.name === body.name)) {
-      return res.status(400).send('Name must be unique');
-    }
+
     const person = {
-      id: Math.floor(Math.random() * 1000),
-      name: body.name,
-      number: body.number
+        id: generateId(),
+        name: body.name,
+        number: body.number,
     };
-    persons.push(person);
+
+    persons = persons.concat(person);
+
     res.json(person);
 });
 
+// Delete a person
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const personIndex = persons.findIndex(person => person.id === id);
-  if (personIndex === -1) {
-    return res.status(404).send(`Person ${id} not found`);
-  }
-  persons.splice(personIndex, 1);
-  res.send(`Person ${id} was deleted`);
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}/api/persons`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
