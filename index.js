19eab31ceb42c06518239d3dcbbe80eb21c26548
plj saveli <saveli.pljasunov@gmail.com>
2024-11-04 10:53:50 +0200

    added dist

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3001
 
  const date = new Date();
- 
+
+ app.use(express.static('dist'))
  app.use(cors());
  app.use(express.json());
  app.use(morgan('tiny'));
