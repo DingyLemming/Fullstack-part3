bec35bd82c4873fda516a49c69804bbeaeed4389
plj saveli <saveli.pljasunov@gmail.com>
2024-11-13 03:48:20 +0200

    database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Person from './models/person.js';
+
+dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('dist'));
@@ -11,71 +14,44 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-1456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
-// Generate unique ID
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0;
-    return maxId + 1;
-};
-
 // Get all persons
 app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
     res.json(persons);
+  });
 });
 
 // Add a new person
 app.post('/api/persons', (req, res) => {
-    const body = req.body;
+  const body = req.body;
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({ 
-            error: 'name or number missing' 
-        });
-    }
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name or number missing' 
+    });
+  }
 
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    };
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-    persons = persons.concat(person);
-
-    res.json(person);
+  person.save().then(savedPerson => {
+    res.json(savedPerson);
+  });
 });
 
 // Delete a person
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'failed to delete person' });
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- models/person.js
+++ models/person.js
@@ -1,34 +1,35 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.set('strictQuery', false)
+dotenv.config();
 
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
-
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
+const Person = mongoose.model('Person', personSchema);
 
-module.exports = mongoose.model('Person', personSchema)
+export default Person;
